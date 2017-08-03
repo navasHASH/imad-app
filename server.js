91eaec3fefa073f5775f40055b16941da5adce78
navasHASH <30633915+navasHASH@users.noreply.github.com>
2017-08-03 06:46:46 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ var title =data.title;
 var template=`<html>
     <head>
         <title>
-           
+           ${title}
         </title>
         <meta name="viewport" content="width-device-width, initial-scale-1"/>
       <link href="/ui/style.css" rel="stylesheet"/>
