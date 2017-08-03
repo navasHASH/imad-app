48d6f4afcf6e3d0ff5f77f10f3d0bcce7d8bd5e2
navasHASH <30633915+navasHASH@users.noreply.github.com>
2017-08-03 07:44:25 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:article-one', function (req, res){
-      res.send(createTemplate(articles[articleone]));
+      res.send(createTemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
